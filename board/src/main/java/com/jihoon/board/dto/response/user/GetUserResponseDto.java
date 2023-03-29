ef118df067eb6d5897eacde9f2ebf55718a18911
nugoori <tig01610@naver.com>
2023-03-29 17:52:28 +0900

    2023-03-29 17:50

---
+++ board/src/main/java/com/jihoon/board/dto/response/user/GetUserResponseDto.java
@@ -0,0 +1,34 @@
+package com.jihoon.board.dto.response.user;
+
+import com.jihoon.board.entity.UserEntity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@ApiModel(value="유저 정보 불러오기 Response Body - data")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetUserResponseDto {
+    @ApiModelProperty(value="유저 이메일", example="yir@akak.com", required=true)
+    private String email;
+    @ApiModelProperty(value="유저 닉네임", example="nugoori", required=true)
+    private String nickname;
+    @ApiModelProperty(value="유저 휴대전화 번호", example="010-0000-0000", required=true)
+    private String telNumber;
+    @ApiModelProperty(value="유저 주소", example="부산광역시 해운대구", required=true)
+    private String address;
+    @ApiModelProperty(value="유저 프로필 이미지 URL", example="Http://~", required=true)
+    private String profile;
+
+    public GetUserResponseDto(UserEntity userEntity) {
+        this.email = userEntity.getEmail();
+        this.nickname = userEntity.getNickname();
+        this.telNumber = userEntity.getTelNumber();
+        this.address = userEntity.getAddress();
+        this.profile = userEntity.getProfile();
+    }
+}
--- board/src/main/java/com/jihoon/board/service/UserService.java
+++ board/src/main/java/com/jihoon/board/service/UserService.java
@@ -2,9 +2,10 @@ package com.jihoon.board.service;
 
 import com.jihoon.board.dto.request.user.PatchProfileDto;
 import com.jihoon.board.dto.response.ResponseDto;
+import com.jihoon.board.dto.response.user.GetUserResponseDto;
 import com.jihoon.board.dto.response.user.PatchProfileResponseDto;
 
 public interface UserService {
-    
     public ResponseDto<PatchProfileResponseDto> patchProfile(String email, PatchProfileDto dto);
+    public ResponseDto<GetUserResponseDto> getUser(String email);
 }
